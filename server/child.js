ac95b7e79f43498f54ab58202826fc0747cbd262
徐媛媛 <yuanyuan.xu@ymm56.com>
2019-03-04 16:05:50 +0800

    add

--- server/child.js
+++ server/child.js
@@ -1,8 +1,31 @@
+let path = require('path')
 let exec = require('child_process').exec
 let absPath = process.cwd()
 let execSync = require("child_process").execSync
 let child = exec("git symbolic-ref --short -q HEAD")
+let curBranch = execSync(`git symbolic-ref --short -q HEAD`, {cwd: absPath}).toString().split('\n')
 let fetchBranch = execSync(`git branch -r`, {cwd: absPath}).toString().split('\n')
+let isGitPath = false
+function hasGitPath() {
+  let pathExec = exec(`find .git`)
+  try{
+    console.log(execSync('find "../.git"', {cwd: absPath}).toString().split('\n'))
+    isGitPath = true
+  }catch (err){
+    console.log(err)
+  }
+  pathExec.stdout.on("data", function (data) {
+    console.log("stdout", data)
+  })
+  pathExec.stderr.on("data", function (data) {
+    console.log("stderr", data)
+  })
+  pathExec.on("close", function (data) {
+    console.log("close", data)
+  })
+}
+hasGitPath()
+console.log(curBranch)
 console.log(fetchBranch)
 console.log(absPath)
 
@@ -17,6 +40,6 @@ child.stdout.on("data", function (data) {
 child.stderr.on("data", function (data) {
   console.log("stderr", data)
 })
-child.on("cluse", function (data) {
+child.on("close", function (data) {
   console.log("close", data)
 })
